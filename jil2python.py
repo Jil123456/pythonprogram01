c5849ef14aca2b301d2ae737cd51164e99a50f26
Jil Patel <jilpatel0310@gmail.com>
2025-01-19 09:56:35 +0530

    Add files via upload

---
+++ jil2python.py
@@ -0,0 +1,50 @@
+# 11. Convert kilogram into gram
+kg=3000
+gram=kg//1000
+print(gram)
+# 12. Convert byte into kb,mb,gb
+byte=4
+kb=byte/1000
+mb=byte/1000000
+gb=byte/1000000000
+print(kb,mb,gb)
+# 13. Convert celcuis into fahrenheit
+c=32
+f=(9/5*c)+32
+print(f)
+# 14. Convert fahrenheit into celcuis
+f=45
+c=5/9*(f-32)
+print(c)
+# 15. Calculate the interest through p,r,n
+p=1000
+r=2
+n=6
+interest=(p*r*n)/100
+print(interest)
+# 16. Calculate the area and perimeter of the square
+side=4
+area_square=side*side
+perimeter_square=4*side
+print(area_square,perimeter_square)
+# 17. Calculate the area and perimeter of the reactangle
+l=2
+b=5
+areaofrect=l*b
+periofrect=2*(l+b)
+print(areaofrect,periofrect)
+# 18. Calculate the area of the circle
+radius=4
+area=3.14*radius*radius
+print(area)
+# 19. Calculate the area of the triangle
+base=2
+height=5
+area=(1/2)*height*base
+print(area)
+# 20. Calculate the net salary
+grosssalary=500
+allowance=(10*grosssalary)/100
+dedution=(3*grosssalary)/100
+netsalary=grosssalary+allowance-dedution
+print(netsalary)
